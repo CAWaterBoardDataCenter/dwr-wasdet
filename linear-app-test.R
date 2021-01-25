d50db549c4e56adcb8736b30a39280803dc7be26
Jeff Yeazell <jyeazell@mac.com>
2021-01-24 17:07:57 -0800

    Resolving with stash

--- linear-app-test.R
+++ linear-app-test.R
@@ -17,6 +17,7 @@ priority_selected <- 1950
 huc8_selected <- "Trinity"
 # scenario_selected <- "Reported Diversions - 2019"
 scenario_selected <- c("Reported Diversions - 2019", "Reported Diversions - 2011")
+<<<<<<< Updated upstream
 
 ## Selections. ----
 
@@ -59,6 +60,15 @@ if(load_from_s3) {
   # Supply data.
   load("./output/dwast-supplies.RData")
 }
+=======
+
+## Load data files. ----
+
+load("./output/dwast-wrinfo-2021-01-13.RData")
+load("./output/dwast-demands-2021-01-13.RData")
+load("./output/dwast-supply-2021-01-15.RData")
+
+>>>>>>> Stashed changes
 
 ## Define color palettes and plot order. ----
 
@@ -77,11 +87,16 @@ wa_demand_order <- ordered(
 wa_demand_pal <- wes_palettes$GrandBudapest1[c(2, 1, 4, 3)]
 names(wa_demand_pal) <- wa_demand_order
 
+<<<<<<< Updated upstream
 # Supply.
 wa_supply_pal <- colorRampPalette(wes_palette("Zissou1")[1:2])(3)
 wa_supply_shapes <- c(15, 16, 17)
 
 ## watershed
+=======
+## watershed
+
+>>>>>>> Stashed changes
 ws_demand <- demand[[huc8_selected]]
 ws_wr_info <- filter(wr_info, huc8_name %in% huc8_selected)
 
@@ -91,6 +106,7 @@ scenario_demand <- filter(ws_demand, scenario %in% scenario_selected)
 
 
 ## Munge scenario demand for plot.
+<<<<<<< Updated upstream
 
 # plot_demand <- scenario_demand %>% 
 #     mutate(fill_color = if_else(priority == "Statement Demand",
@@ -237,6 +253,56 @@ g <- g + theme(
     axis.title.x = element_blank())
 
 g
+=======
+
+plot_demand <- scenario_demand %>% 
+    mutate(fill_color = if_else(priority == "Statement Demand",
+                                "Statement Demand",
+                                if_else(priority == "Statement Demand",
+                                        "Statement Demand",
+                                        if_else(p_year >= priority_selected,
+                                                "Junior Post-14", "Post-14"))),
+           fill_color = ordered(fill_color, levels = wa_demand_order)) %>% 
+    group_by(scenario, rept_date, fill_color) %>% 
+    summarise(demand_daily_af = sum(demand_daily_af, na.rm = TRUE),
+              demand_cfs = sum(demand_cfs, na.rm = TRUE),
+              .groups = "drop")
+
+
+
+## Render plot. ----
+
+g <- ggplot(data = plot_demand,
+            aes(x = rept_date,
+                y = demand_daily_af,
+                group = fill_color,
+                fill = fill_color)) 
+
+g <- g + geom_area(position = "stack")
+
+g <- g + scale_fill_manual(name = "Demand type:",
+                           values = wa_demand_pal,
+                           labels = c(paste(priority_selected, 
+                                            "& Junior Post-14 Demand"),
+                                      "Senior Post-14 Demand",
+                                      "Statement Demand"))
+
+g <- g + labs(y = "Acre-Feet/Day")
+
+g <- g + theme(
+    legend.position = "bottom",
+    strip.text.x = element_text(size = rel(1.5)),
+    axis.title = element_text(size = rel(1.2)),
+    axis.text = element_text(size = rel(1.2)),
+    legend.text = element_text(size = rel(1.2)),
+    legend.title = element_text(size = rel(1.2)),
+    axis.title.x = element_blank())
+
+g <- g + facet_wrap(facets = vars(plot_demand$scenario),
+             nrow = length(unique(plot_demand$scenario)))
+
+
+>>>>>>> Stashed changes
 
 
 
